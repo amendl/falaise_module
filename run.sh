b1058ec456a12b2a8927207b4b2d6b1d2d19208b
Adam Mendl <amendl@cca014.in2p3.fr>
2024-02-06 15:16:10 +0100

    add t

---
+++ run.sh
@@ -0,0 +1,3 @@
+source /sps/nemo/scratch/amendl/AI/physical_simulation/load_falaise4.sh
+flreconstruct -i /sps/nemo/scratch/amendl/AI/physical_simulation/MiModule/testing_products/test-reco.brio -p pipeline.conf
+
